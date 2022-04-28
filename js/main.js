f8ba9ef31876d0bb4af9f4450b0dff579f9c34d7
psh827 <dhdp564@gmail.com>
2022-04-29 00:39:41 +0900

    mainPage기능추가
    좋아요기능추가

--- js/main.js
+++ js/main.js
@@ -1,13 +1,70 @@
-$(function(){
-    let myid = $('.myprofile .article-user span').text()
-    $('.reple-area textarea').on("propertychange change keyup paste input",function(){
-        if($(this).val() != ''){
-            $(this).next().prop('disabled', false);
-        }
-    })
+$(function () {
+  let myid = $(".myprofile .article-user span").text();
+  //textarea에 변화가 생기면 게시버튼이 활성화 되는 버튼
+  $(".reple-area textarea").on(
+    "propertychange change keyup paste input",
+    function () {
+      if ($(this).val() != "") {
+        $(this).next().prop("disabled", false);
+      } else {
+        $(this).next().prop("disabled", true);
+      }
+    }
+  );
+  //게시버튼 클릭시 댓글 추가기능
+  $("button.push-btn").click(function () {
+    $(this)
+      .parent()
+      .prev()
+      .prev()
+      .append(
+        '<p class="mycomment"><a href="#" class="user-id">' +
+          myid +
+          '</a><span class="comment-text">' +
+          $(this).prev().val() +
+          '</span><span class="comment-logo"><i class="fa-regular fa-heart fa-sm"></i></span></p>'
+      );
+    $(this).prev().val("");
+  });
+  //좋아요 클릭시 빨간색으로 변경, 좋아요 숫자 변경
+  $(document).on("click", ".fa-heart", function () {
+    //좋아요 갯수 가져오기
+    let likecount = "";
+    //두번째 세번째 댓글 좋아요오류(좋아요가 눌리지않는 오류)를 위한 예외 처리.
+    if ($(this).hasClass("fa-sm") != true) {
+      likecount = $(this)
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .next()
+        .find(".like")
+        .find("span")
+        .text()
+        .split(" ")[1]
+        .split("개")[0];
+    }
+    //span tag 지정 변수
+    let spantext = $(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .next()
+      .find(".like")
+      .find("span");
+    //좋아요 갯수 숫자화
+    likecount = Number(likecount);
 
-    $('button.push-btn').click(function(){
-       $(this).parent().prev().prev().append('<p class="mycomment"><span class="user-id">'+myid+'</span><span class="comment-text">'+$(this).prev().val()+'</span></p>');
-       $(this).prev().val('');
-    })
-})
+    $(this).toggleClass("fa-solid");
+    if ($(this).hasClass("fa-solid")) {
+      $(this).css("color", "red");
+      if ($(this).hasClass("fa-xl")) {
+        spantext.text("좋아요 " + (likecount + 1) + "개");
+      }
+    } else {
+      $(this).css("color", "black");
+      spantext.text("좋아요 " + (likecount - 1) + "개");
+    }
+  });
+});
--- js/text_ellipsis.js
+++ js/text_ellipsis.js
@@ -1,53 +1,52 @@
-$(function(){
-    let elem = $('.comment-main-text span');
-    const showText = 20;
-    let moreBtnText = "더보기";
-    let lessBtnText = "간략히";
-    // elem = document.body.getElementsByClassName('more-text');
-    // for(let i = 0; i < elem.length; i++){
-    //     let content = elem[i].html();
-    // }
+$(function () {
+  let elem = $(".comment-main-text span");
+  const showText = 20;
+  let moreBtnText = "더보기";
+  let lessBtnText = "간략히";
+  // elem = document.body.getElementsByClassName('more-text');
+  // for(let i = 0; i < elem.length; i++){
+  //     let content = elem[i].html();
+  // }
 
-    elem.each(function(i){
-        console.log(i)
-        let content = $(this).html();
-        let contentLeng = content.length;
-        let lessText = content.substr(0, showText);
-        let moreText = content.substr(showText, contentLeng);
-        let changeHtml = '';
-        if (showText < contentLeng) {
-            changeHtml = `${lessText} <span>...</span><span class="more-content"><span>${moreText}</span><a href="#" class="more-btn active"><span>${moreBtnText}</span></a></span>`;
-            $(this).html(changeHtml);
-            if( i == 0 ){
-                console.log(i)
-                console.log($(this))
-                console.log($(this).attr("class"))
-                $(this).html(changeHtml)
-            }else if( i == 1 ){
-                console.log(i)
-                console.log($(this))
-                console.log($(this).attr("class"))
-                $(this).html(changeHtml)
-            }else{
-                console.log(i)
-                console.log($(this))
-                console.log($(this).attr("class"))
-                $(this).html(changeHtml)
-            }
-        }
-    });
+  elem.each(function (i) {
+    console.log(i);
+    let content = $(this).html();
+    let contentLeng = content.length;
+    let lessText = content.substr(0, showText);
+    let moreText = content.substr(showText, contentLeng);
+    let changeHtml = "";
+    if (showText < contentLeng) {
+      changeHtml = `${lessText} <span>...</span><span class="more-content"><span>${moreText}</span><a class="more-btn active"><span>${moreBtnText}</span></a></span>`;
+      $(this).html(changeHtml);
+      if (i == 0) {
+        console.log(i);
+        console.log($(this));
+        console.log($(this).attr("class"));
+        $(this).html(changeHtml);
+      } else if (i == 1) {
+        console.log(i);
+        console.log($(this));
+        console.log($(this).attr("class"));
+        $(this).html(changeHtml);
+      } else {
+        console.log(i);
+        console.log($(this));
+        console.log($(this).attr("class"));
+        $(this).html(changeHtml);
+      }
+    }
+  });
 
-        $('.more-btn').click(function(e){
-            let check = $(this).hasClass('active');
-            if(check){
-                $(this).removeClass('active');
-                $(this).html(``);
-            }
-            else{
-                $(this).addClass('active');
-                $(this).html(`<span>${moreBtnText}</span>`);
-            }
-            $(this).prev().toggle();
-            $(this).parent().prev().toggle();
-           });
+  $(".more-btn").click(function (e) {
+    let check = $(this).hasClass("active");
+    if (check) {
+      $(this).removeClass("active");
+      $(this).html(``);
+    } else {
+      $(this).addClass("active");
+      $(this).html(`<span>${moreBtnText}</span>`);
+    }
+    $(this).prev().toggle();
+    $(this).parent().prev().toggle();
+  });
 });
